d34bc8044bca3e57755da041509a41366ff0c819
bendmorris <ben@bendmorris.com>
2013-06-21 09:49:56 -0400

    Updating annotate function. Annotations are loaded using SPARQL 1.1 UPDATE. To be concise, the ?tree variable can be used to refer to the tree being annotated.

--- annotate.py
+++ annotate.py
@@ -1,6 +1,23 @@
 class Annotatable:
-    def annotate(self, tree_uri, annotation_file):
+    def annotate(self, tree_uri, annotations=None, annotation_file=None):
         '''Annotate tree with annotations from RDF file.'''
         cursor = self.get_cursor()
         
-        # TODO: rewrite without redland
+        if annotation_file and not annotations:
+            with open(annotation_file) as input_file:
+                annotations = input_file.read()
+        
+        insert_stmt = '''sparql
+PREFIX obo: <http://purl.obolibrary.org/obo/>
+
+WITH <%s>
+INSERT {
+    %s
+}
+WHERE {
+    ?tree obo:CDAO_0000148 [] .
+}
+        ''' % (tree_uri, annotations)
+        print insert_stmt
+        
+        cursor.execute(insert_stmt)
